d2d2da3815c252ea3341a83344f5ef12ab5fa662
David Schäfer <david@schaefer.su>
2024-11-18 12:30:11 +0100

    Fix formatting

--- helloWorld.py
+++ helloWorld.py
@@ -1,15 +1,18 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt6.QtCore import Qt
 
+
 class HelloWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Hello World")
         label = QLabel("Hello World")
-        label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
-
+        label.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
+        )
         self.setCentralWidget(label)
+
 
 app = QApplication([])
 window = HelloWindow()
