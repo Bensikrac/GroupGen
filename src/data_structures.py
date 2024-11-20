750e6c9e6344261582a1da36413c2e0181e76846
Benedek Major <benedek@major.onl>
2024-11-20 16:40:44 +0100

    fix: members and name private, re-add convenience functions

--- src/data_structures.py
+++ src/data_structures.py
@@ -1,40 +1,56 @@
 class Participant:
     """data structure, which represents a participant by storing name and attribute list of an individual participant"""
 
-    name: str
+    __name: str
     attributes: dict[str, str]
     """attributes are mapped using attribute name ex. 'course' -> 'math' """
 
     def __init__(self, name: str):
         """Creates a participant with the name set. For groups Names should be unique"""
-        self.name = name
+        self.__name = name
 
     def __init__(self, name: str, attributes: dict[str, str]):
         """Creates a new participant with the given name and attributes. Attributes are given in a dict of str->List[str]"""
-        self.name = name
+        self.__name = name
         self.attributes = attributes
+
+    def get_name(self) -> str:
+        """returns the name of the participant"""
+        return self.__name
+
+    def get_attribute(self, attribute: str) -> str:
+        """returns the attribute value of the given attribute for the participant"""
+        return self.attributes[attribute]
+
+    def set_attribute(self, attribute: str, value: str):
+        """sets the given attribute to the given value for the participant"""
+        self.attributes[attribute] = value
 
     def __eq__(self, other) -> bool:
         """Compares if two participants are equal, based on their name"""
-        return self.name == other.name
+        return self.__name == other.__name
 
 
 class Group:
     """data structure which represents a group of participants"""
 
-    members: list[Participant]
+    __members: list[Participant]
 
     def __init__(self):
         """creates an empty group"""
 
     def __init__(self, members: list[Participant]):
         """creates a group with the given participants"""
-        self.members = members
+        self.__members = members
+
+    def get_members(self) -> list[Participant]:
+        """returns all participants of the given group"""
+        return self.__members
 
     def add_participant(self, participant: Participant):
         """adds the given participant to the group"""
-        self.members.append(participant)
+        self.__members.append(participant)
 
     def remove_participant(self, participant: Participant):
         """removes the given participant from the group"""
-        self.members.remove(participant)
+        self.__members.remove(participant)
