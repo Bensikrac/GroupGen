29c472d595751a5940da5087e5ff0445e93659a2
David Schäfer <david@schaefer.su>
2024-11-25 22:11:49 +0100

    Add data manager, change data structures

---
+++ src/data_manager.py
@@ -0,0 +1,88 @@
+from typing import Any
+from data_structures import AttributeUID, AttributeValueUID, ParticipantUID, UID
+
+
+class DataManager:
+    """Manage attribute names and IDs"""
+
+    """
+    __attribute_value_mapping: dict[AttributeUID, set[AttributeValueUID]]
+    __value_attribute_mapping: dict[AttributeValueUID, AttributeUID]
+    __participant_mapping: dict[
+        ParticipantUID, tuple[set[AttributeUID], set[AttributeValueUID]]
+    ]
+    """
+
+    __participant_uid_offset: int
+    """The first ParticipantUID"""
+    __data_uid_offsets: list[int]
+    """The first n-1 entries of this list are each the first AttributeValueUID of the corresponding attribute, the last entry is the first AttributeUID"""
+
+    __data_uids: list[list[AttributeValueUID] | list[AttributeUID]]
+    """A list containing a list per attribute,
+          the inner lists contain the uid of the value each participant has in that attribute in order, the last entry of the outer list is a list of all attribute uids in order"""
+
+    __uid_name_mapping: dict[UID, str]
+    __name_uid_mapping: dict[tuple[str, AttributeUID], UID]
+
+    def __map_value_name(
+        self, uid: UID, attribute_uid: AttributeUID, name: str
+    ) -> None:
+        """Maps the uid of a given value of a given attribute to the name of the value"""
+        self.__uid_name_mapping[uid] = name
+        self.__name_uid_mapping[(name, attribute_uid)] = uid
+
+    def __init__(self, data: list[list[str]]) -> None:
+        """
+        Initializer, stores the given data internally using uids.
+
+        :param list[list[str]] data: A list containing a list per attribute,
+          the inner lists contain the value each participant has in that attribute in order, the last entry of the outer list is a list of all attribute names in order
+        """
+        self.__data_uids = [[AttributeValueUID()] * len(data[0])] * (len(data) - 1) + [
+            [AttributeUID()] * len(data[-1])
+        ]  # initialize __data_uids with correct lengths (should be the same as data)
+
+        self.__data_uid_offsets[-1] = len(data[0]) * (len(data) - 1)
+        self.__participant_uid_offset = self.__data_uid_offsets[-1] + len(data[-1])
+
+        current_uid: UID = self.__data_uid_offsets[-1]
+        for i in range(len(data[-1])):
+            # self.__attribute_value_mapping[current_uid] = set()
+            self.__uid_name_mapping[current_uid] = data[-1][i]
+            self.__name_uid_mapping[(data[-1][i], -1)] = current_uid
+            self.__data_uids[-1][i] = current_uid
+            current_uid += 1
+        current_uid: UID = 0
+        for i in range(len(data) - 1):
+            self.__data_uid_offsets.insert(0, current_uid)
+            current_participant_uid: ParticipantUID = self.__participant_uid_offset
+            for j in range(len(data[i])):
+                if (
+                    data[i][j],
+                    self.__data_uid_offsets[-1] + i,
+                ) not in self.__name_uid_mapping:
+                    self.__map_value_name(
+                        current_uid, self.__data_uid_offsets[-1] + i, data[i][j]
+                    )
+                    current_uid += 1
+                self.__data_uids[i][j] = self.__name_uid_mapping[data[i][j]]
+
+                current_participant_uid += 1
+
+    def get_values_by_attribute(self, uid: AttributeUID) -> set[AttributeValueUID]:
+        """Returns a set containing all the uids of all the values the attribute with a given uid can have"""
+        return set(self.__data_uids[uid - self.__data_uid_offsets[-1]])
+
+    """
+    def get_attribute_by_value(self, uid: AttributeValueUID) -> AttributeUID:
+        return self.__value_attribute_mapping[uid]
+    """
+
+    def get_value_by_participant(
+        self, participant: ParticipantUID, attribute: AttributeUID
+    ) -> AttributeValueUID:
+        """Returns the uid of the value of the attribute of a given uid for the participant of a given uid"""
+        return self.__data_uids[attribute - self.__data_uid_offsets[-1]][
+            participant - self.__participant_uid_offset
+        ]
--- src/data_structures.py
+++ src/data_structures.py
@@ -1,56 +1,6 @@
-class Participant:
-    """data structure, which represents a participant by storing name and attribute list of an individual participant"""
-
-    __name: str
-    attributes: dict[str, str]
-    """attributes are mapped using attribute name ex. 'course' -> 'math' """
-
-    def __init__(self, name: str):
-        """Creates a participant with the name set. For groups Names should be unique"""
-        self.__name = name
-
-    def __init__(self, name: str, attributes: dict[str, str]):
-        """Creates a new participant with the given name and attributes. Attributes are given in a dict of str->List[str]"""
-        self.__name = name
-        self.attributes = attributes
-
-    def get_name(self) -> str:
-        """returns the name of the participant"""
-        return self.__name
-
-    def get_attribute(self, attribute: str) -> str:
-        """returns the attribute value of the given attribute for the participant"""
-        return self.attributes[attribute]
-
-    def set_attribute(self, attribute: str, value: str):
-        """sets the given attribute to the given value for the participant"""
-        self.attributes[attribute] = value
-
-    def __eq__(self, other) -> bool:
-        """Compares if two participants are equal, based on their name"""
-        return self.__name == other.__name
-
-
-class Group:
-    """data structure which represents a group of participants"""
-
-    __members: list[Participant]
-
-    def __init__(self):
-        """creates an empty group"""
-
-    def __init__(self, members: list[Participant]):
-        """creates a group with the given participants"""
-        self.__members = members
-
-    def get_members(self) -> list[Participant]:
-        """returns all participants of the given group"""
-        return self.__members
-
-    def add_participant(self, participant: Participant):
-        """adds the given participant to the group"""
-        self.__members.append(participant)
-
-    def remove_participant(self, participant: Participant):
-        """removes the given participant from the group"""
-        self.__members.remove(participant)
+type UID = int
+type AttributeUID = UID
+type AttributeValueUID = UID
+type ParticipantUID = UID
+type Group = set[ParticipantUID]
+type Round = list[Group]
