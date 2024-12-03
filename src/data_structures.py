11f6bb2d8bcb6190511a1bc818ec5c01e3e43e3b
d01w <149610830+d01w@users.noreply.github.com>
2024-12-03 09:26:59 +0100

    data structure updated

--- src/data_structures.py
+++ src/data_structures.py
@@ -2,37 +2,57 @@
 
 
 class Participant:
-    """data structure, which represents a participant by storing name and attribute list"""
+    """data structure, which represents a participant by storing unique id and attribute list of an individual participant
 
-    __name: str
+    :param uid: unique id for participant
+    :param attributes: list of attributes for participants
+    """
+
+    __uid: int
     attributes: dict[str, str]
-    """attributes are mapped using attribute name ex. 'course' -> 'math' """
+    # attributes are mapped using attribute name ex. 'course' -> 'math'
 
-    def __init__(self, name: str, attributes: dict[str, str] = None):
-        """Creates a participant with the name set. For groups Names should be unique"""
+    def __init__(self, uid: str, attributes: dict[str, str] = None):
         if attributes is None:
             attributes = {}
-        self.__name = name
+        self.__uid = uid
         self.attributes = attributes
 
-    def get_name(self) -> str:
-        """returns the name of the participant"""
-        return self.__name
+    def get_uid(self) -> str:
+        """Returns the uid of the participant.
+
+        :return: unique id of the participant
+        """
+        return self.__uid
 
     def get_attribute(self, attribute: str) -> str:
-        """returns the attribute value of the given attribute for the participant"""
+        """Returns the attribute value of the given attribute for the participant.
+
+        :param attribute: attribute of which the value is returned
+
+        :return: attribute value of the given attribut for the participant
+        """
         return self.attributes[attribute]
 
-    def set_attribute(self, attribute: str, value: str):
-        """sets the given attribute to the given value for the participant"""
+    def set_attribute(self, attribute: str, value: str) -> None:
+        """Sets the given attribute to the given value for the participant.
+
+        :param attribute: attribute to be set for participant (already existing)
+        :param value: value given to the given attribute
+        """
         self.attributes[attribute] = value
 
     def __eq__(self, other) -> bool:
-        """Compares if two participants are equal, based on their name"""
-        return self.__name == other.get_name()
+        return self.__uid == other.__uid
 
     def __repr__(self) -> str:
-        return "Name: " + self.__name + " Attribute: " + str(self.attributes)
+        return "Participant(" + self.__uid + ", " + str(self.attributes) + ")"
+
+    def __str__(self) -> str:
+        return "Name: " + self.__uid + " Attribute: " + str(self.attributes)
+
+    def __hash__(self) -> int:
+        return self.__uid
 
 
 type Group = set[Participant]
