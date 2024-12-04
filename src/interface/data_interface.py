87b09e2ca19eed9db9da890cefafa0bc5d295b1a
Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>
2024-12-04 16:49:02 +0100

    rewrite data interface (again)
    Signed-off-by: Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>

--- src/interface/data_interface.py
+++ src/interface/data_interface.py
@@ -1,109 +1,147 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
+from typing import Protocol
 
 
-class IDataInput[T](metaclass=ABCMeta):
-    """Abstract Base Class for uniformly getting data."""
+class IDataInput[T](Protocol):
+    """Protocol for data input."""
 
     @abstractmethod
     def get(self) -> T:
-        """Get arbitrary data of type :class:`T`
+        """Get arbitrary data of type :class:`T`.
 
-        :return: Data of type :class:`T`
-
-        :meta public:
+        :return: Arbitrary data
         """
         pass
 
 
-class IDataInputTransformer[To, From](IDataInput[To], metaclass=ABCMeta):
-    """Abstract Base Class for transforming input data using an uniform interface.
+class IDataOutput[T](Protocol):
+    """Protocol for data output."""
 
-    Implements :class:`IDataInput[To]`.
-    """
+    @abstractmethod
+    def put(self, x: T) -> None:
+        """Put arbitrary data of :class:`T`.
 
-    __data_input: IDataInput[From]
-
-    def __init__(self, data_input: IDataInput[From]) -> None:
-        """Initialize :class:`IDataInputTransformer[To, From]`.
-
-        :param data_input: The :class:`IDataInput[From]` the transformer will wrap around
-
-        :meta public:
+        :param x: Arbitrary data to put
         """
-        self.__data_input = data_input
+        pass
+
+
+class IDataIO[I, O](IDataInput[I], IDataOutput[O], Protocol):
+    """Combined Protocol for data input/output."""
+
+    pass
+
+
+class IDataInputTransformer[From, To](Protocol):
+    """Protocol for input transformers."""
 
     @staticmethod
     @abstractmethod
-    def transform_get(x: From) -> To:
-        """Transform input data of type :class:`From` to data of type :class:`To`.
+    def transform_get(self, x: From) -> To:
+        """Transform arbitrary data of :class:`From` to :class:`To`.
 
-        :param x: Data to transform, coming from wrapped :func:`IDataInput[From].get`
+        :param x: Arbitrary data
 
-        :return: Transformed data.
-
-        :meta public:
+        :return: Transformed data
         """
         pass
 
+
+class IDataOutputTransformer[From, To](Protocol):
+    """Protocol for output transformers."""
+
+    @staticmethod
+    @abstractmethod
+    def transform_put(self, x: From) -> To:
+        """Transform arbitrary data of :class:`From` to :class:`To`.
+
+        :param x: Arbitrary data
+
+        :return: Transformed data
+        """
+        pass
+
+
+class ITransformedDataInput[From, To](
+    IDataInput[To], IDataInputTransformer[From, To], Protocol
+):
+    """Protocol for transformed data input.
+
+    :param data_input: The underlying :class:`IDataInput` to which :func:`get` is forwarded
+    """
+
+    __data_input: type[IDataInput[From]]
+
+    def __init__(self, data_input: type[IDataInput[From]]) -> None:
+        self.__data_input = data_input
+
     def get(self) -> To:
-        """Get transformed data of type :class:`To`
+        """Get arbitrary data of :class:`From`, transformed to :class:`To`.
 
-        :return: Data of type :class:`From`, transformed to :class:`To` using :func:`transform_get`
-
-        :meta public:
+        :return: Transformed data
         """
         return self.transform_get(self.__data_input.get())
 
 
-class IDataOutput[T](metaclass=ABCMeta):
-    """Abstract Base Class for uniformly putting data."""
+class ITransformedDataOutput[From, To](
+    IDataOutput[From], IDataOutputTransformer[From, To], Protocol
+):
+    """Protocol for transformed data output.
 
-    @abstractmethod
-    def put(self, x: T) -> None:
-        """Put arbitrary data of type :class:`T`
-
-        :return: Data of type :class:`T`
-
-        :meta public:
-        """
-        pass
-
-
-class IDataOutputTransformer[From, To](IDataOutput[From], metaclass=ABCMeta):
-    """Abstract Base Class for transforming output data using an uniform interface.
-
-    Implements :class:`IDataInput[From]`.
+    :param data_output: The underlying :class:`IDataOutput` to which :func:`put` is forwarded
     """
 
-    __data_output: IDataOutput[To]
+    __data_output: type[IDataOutput[To]]
 
-    def __init__(self, data_output: IDataOutput[To]) -> None:
-        """Initialize :class:`IDataOutputTransformer[From, To]`.
-
-        :param data_output: The :class:`IDataOutput[To]` the transformer will wrap around
-
-        :meta public:
-        """
+    def __init__(self, data_output: type[IDataOutput[To]]) -> None:
         self.__data_output = data_output
 
-    @staticmethod
-    @abstractmethod
-    def transform_put(x: From) -> To:
-        """Transform output data of type :class:`From` to data of type :class:`To`.
-
-        :param x: Data to transform
-
-        :return: Transformed data, forwarded to wrapped :func:`IDataOutput[To].put`
-
-        :meta public:
-        """
-        pass
-
     def put(self, x: From) -> None:
-        """Put data of type :class:`From` transformed to type :class:`To`
+        """Put arbitrary data of :class:`From`, transformed to :class:`To`.
 
-        :param x: Data of type :class:`From` to transform and put
-
-        :meta public:
+        :param x: Data to transform and put
         """
         return self.__data_output.put(self.transform_put(x))
+
+
+class ITransformedDataIO[IFrom, ITo, OFrom, OTo](
+    IDataIO[ITo, OFrom],
+    IDataInputTransformer[IFrom, ITo],
+    IDataOutputTransformer[OFrom, OTo],
+    Protocol,
+):
+    """Combined Protocol for transformed data input/output.
+
+    :param data_io: The underlying :class:`IDataIO` to which :func:`get` and :func:`put` is forwarded
+    """
+
+    __data_io: type[IDataIO[IFrom, OTo]]
+
+    def __init__(self, data_io: type[IDataIO[IFrom, OTo]]) -> None:
+        self.__data_io = data_io
+
+    def get(self) -> ITo:
+        """Get arbitrary data of :class:`IFrom`, transformed to :class:`ITo`.
+
+        :return: Transformed data
+        """
+        return self.transform_get(self.__data_io.get())
+
+    def put(self, x: OFrom) -> None:
+        """Put arbitrary data of :class:`OFrom`, transformed to :class:`OTo`.
+
+        :param x: Data to transform and put
+        """
+        return self.__data_io.put(self.transform_put(x))
+
+
+class ISymmetricDataIO[T](IDataIO[T, T], Protocol):
+    """Convenience Protocol for symmetric data input/output."""
+
+    pass
+
+
+class ISymmetricTransformedDataIO[P, E](ITransformedDataIO[E, P, P, E]):
+    """Convenience Protocol for symmetric transformed data input/output."""
+
+    pass
