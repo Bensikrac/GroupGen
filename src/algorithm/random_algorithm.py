5bd03fb16cbdc4d86ede984e1be0ccf4ef90f1d5
David Schäfer <david@schaefer.su>
2024-12-05 14:54:35 +0100

    Clean up code and make it conform to style guide

--- src/algorithm/random_algorithm.py
+++ src/algorithm/random_algorithm.py
@@ -1,37 +1,52 @@
+"""Random algorithm module"""
+
 from copy import copy
 from math import ceil
 from random import Random
 import random
-from data_structures import Group, Participant
+from data_structures import Assignment, Group, Participant
 
 
 class RandomAlgorithm:
-    """Calculates group assignments using randomness"""
+    """Calculates group assignments using randomness
+
+    :param random: An instance of Random that will be used
+    instead of an automatically generated one as a source or randomness, defaults to None
+    """
 
     __random: Random
 
-    def __init__(self):
-        self.__random = Random()
-
-    def __init__(self, random: Random):
-        self.__random = random
+    def __init__(self, random: Random = None):
+        if random is None:
+            self.__random = Random()
+        else:
+            self.__random = random
 
     def find_assignment(
-        self, participants: set[Participant], groups_per_round: int, rounds: int
-    ) -> list[list[Group]]:
-        """Returns a randomly generated group assignment"""
-        max_group_size: int = ceil(len(participants) / groups_per_round)
-        assignment: list[list[Group]] = list()
-        for i in range(rounds):
+        self, participants: set[Participant], groups_per_iteration: int, iterations: int
+    ) -> Assignment:
+        """Return a randomly generated group assignment.
+
+        :param participants: The set of participants to distribute into groups
+        :param groups_per_round: The number of goups per iteration
+        :param iterations: The number of iterations to generate groups for
+
+        :return: The generated assignment
+        """
+        max_group_size: int = ceil(len(participants) / groups_per_iteration)
+        assignment: Assignment = list()
+        for i in range(iterations):
             remaining_participants: set[Participant] = copy(participants)
             round: list[Group] = list()
-            for j in range(groups_per_round):
-                group: Group = Group()
+            for j in range(groups_per_iteration):
+                group: Group = set()
                 for k in range(max_group_size):
+                    if len(remaining_participants) is 0:
+                        break
                     participant: Participant = self.__random.choice(
                         tuple(remaining_participants)
                     )
-                    group.add_participant(participant)
+                    group.add(participant)
                     remaining_participants.discard(participant)
                 round.append(group)
             assignment.append(round)
--- src/algorithm/simulated_annealing_algorithm.py
+++ src/algorithm/simulated_annealing_algorithm.py
@@ -1,42 +1,57 @@
+"""Simulated Annealing algorithm module"""
+
 from copy import copy
 from math import exp
 from random import Random
 from typing import Any
 from algorithm.objective_function import ObjectiveFunction
 from algorithm.random_algorithm import RandomAlgorithm
-from data_structures import Group, Participant
+from data_structures import Assignment, Iteration, Group, Participant
 
 
 class SimulatedAnnealingAlgorithm:
-    """Calculates group assignments using simulated annealing"""
+    """Contains the calculations for generating group assignments using simulated annealing.
+
+    :param attributes: A list of attributes that are considered for optimization
+    :param random: An instance of Random that will be used
+    instead of an automatically generated one as a source or randomness, defaults to None
+    """
 
     __random: Random
     attributes: list[str]
 
-    def __init__(self, attributes: list[str] = []):
-        self.__random = Random()
-        self.attributes = attributes
-
-    def __init__(self, random: Random, attributes: list[str] = []):
-        self.__random = random
+    def __init__(self, attributes: list[str], random: Random = None):
+        if random is None:
+            self.__random = Random()
+        else:
+            self.__random = random
         self.attributes = attributes
 
     def find_assignment(
         self,
         participants: set[Participant],
-        groups_per_round: int,
-        rounds: int,
-        max_iterations: int,
-    ) -> list[list[Group]]:
-        """Returns a group assignment generated using simulated annealing"""
+        groups_per_iteration: int,
+        iterations: int,
+        max_cycles: int,
+    ) -> Assignment:
+        """Return a group assignment generated using simulated annealing.
+
+        :param participants: The set of participants to distribute into groups
+        :param groups_per_iteration: the number of groups in each iteration
+        :param iterations: The total number of iterations
+        :param max_cycles: The maximum number of times
+        the algorithm will iteratively improve the assignment
+
+        :return: the generated assignment
+        """
         random: RandomAlgorithm = RandomAlgorithm(self.__random)
-        assignment: list[list[Group]] = random.find_assignment(
-            participants, groups_per_round, rounds
+        assignment: Assignment = random.find_assignment(
+            participants, groups_per_iteration, iterations
         )
-        objective: ObjectiveFunction = ObjectiveFunction(self.attributes, assignment)
-        for i in range(max_iterations):
-            temperature: int = self.get_temperature(1 - (i + 1) / max_iterations)
-            neighbor: list[list[Group]] = self.find_neighbor(assignment)
+        objective: ObjectiveFunction = ObjectiveFunction(self.attributes)
+        for i in range(max_cycles):
+            temperature: int = self.get_temperature(1 - (i + 1) / max_cycles)
+            neighbor: Assignment = self.find_neighbor(assignment)
             if (
                 self.get_step_probability(
                     objective.calculate_mix_cost(assignment),
@@ -49,52 +64,70 @@ class SimulatedAnnealingAlgorithm:
         return assignment
 
     def get_temperature(self, progress: float) -> float:
-        """Calculates the temperature for a given progress through the annealing process"""
+        """Calculate the temperature for a given progress through the annealing process.
+
+        :param progress: The fraction of the maximum cycles for the process that has passed
+
+        :return: The temperature value
+        """
         return 100 * 0.1 ** (progress) - 10
 
-    def find_neighbor(self, assignment: list[list[Group]]) -> list[list[Group]]:
-        """Returns a random group assignment that is one swap removed from the given assignment"""
+    def find_neighbor(self, assignment: Assignment) -> Assignment:
+        """Return a random group assignment that is one swap removed from the given assignment.
+
+        :param assignment: The assignment to find a neighbor for
+
+        :return: The found neighbor
+        """
         neighbor = self.__half_deep_copy(assignment)
-        round: int = self.__random.randrange(len(assignment))
-        group_index_1: int = self.__random.randrange(len(assignment[round]))
+        index = self.__random.randrange(len(assignment))
+        iteration: Iteration = assignment[index]
+        group_index_1: int = self.__random.randrange(len(assignment[iteration]))
         participant_index_1: int = self.__random.randrange(
-            len(assignment[round][group_index_1].get_members)
+            len(iteration[group_index_1].get_members)
         )
 
         group_index_2: int = -1
         participant_index_2: int = -1
         while (group_index_2 is -1) or (group_index_1 is group_index_2):
-            group_index_2 = self.__random.randrange(len(assignment[round]))
+            group_index_2 = self.__random.randrange(len(iteration))
             participant_index_2 = self.__random.randrange(
-                len(assignment[round][group_index_2].get_members)
+                len(iteration[group_index_2].get_members)
             )
 
-        participant_1: Participant = assignment[round][group_index_1].get_members()[
-            participant_index_1
-        ]
-        participant_2: Participant = assignment[round][group_index_2].get_members()[
-            participant_index_2
-        ]
-        assignment[round][group_index_1].remove_participant(participant_1)
-        assignment[round][group_index_1].add_participant(participant_2)
-        assignment[round][group_index_2].remove_participant(participant_2)
-        assignment[round][group_index_2].add_participant(participant_1)
+        participant_1: Participant = iteration[group_index_1][participant_index_1]
+        participant_2: Participant = iteration[group_index_2][participant_index_2]
+        neighbor[index][group_index_1].discard(participant_1)
+        neighbor[index][group_index_1].add(participant_2)
+        neighbor[index][group_index_2].discard(participant_2)
+        neighbor[index][group_index_2].add(participant_1)
 
-        return assignment
+        return neighbor
 
     def __half_deep_copy(self, object: Any) -> Any:
-        """Returns a recursive deep copy of the given object that stops at any element that isn't a Group, set or list"""
+        """Return a recursive deep copy of the given object
+        that stops at any element that isn't a set or list.
+
+        :param object: The object to copy
+
+        :return: The copy
+        """
         if isinstance(object, list) or isinstance(object, set):
             return [self.__half_deep_copy(item) for item in object]
-        elif isinstance(object, Group):
-            return Group([self.__half_deep_copy(item) for item in object.get_members()])
         else:
             return copy(object)
 
     def get_step_probability(
         self, energy_old: float, energy_new: float, temperature: float
     ) -> float:
-        """Returns the probability between 0 and 1 of taking a step from an assignment with the givven old energy to one with the given new energy"""
+        """Return the probability of taking a step from an assignment to another.
+
+        :param energy_old: The cost of the old assignment
+        :param energy_new: The cost of the new assignment
+        :param temperature: The current temperature
+
+        :return: The probability of taking the step, based on the Metropolis criterion
+        """
         if energy_new < energy_old:
             return 1
         else:
