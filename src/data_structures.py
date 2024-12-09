e20b976ef84074530c87f36ed349551b1f693972
Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>
2024-12-09 17:08:44 +0100

    fix data structures
    Signed-off-by: Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>

--- src/data_structures.py
+++ src/data_structures.py
@@ -1,9 +1,10 @@
 """Data structures module"""
 
+from typing import TypeVar
+
 
 class Participant:
-    """data structure, which represents a participant by storing unique id and
-    attribute list of an individual participant
+    """Structure representing a participant
 
     :param uid: unique id for participant
     :param attributes: list of attributes for participants
@@ -11,46 +12,45 @@ class Participant:
 
     __uid: int
     attributes: dict[str, str]
-    # attributes are mapped using attribute name ex. 'course' -> 'math'
+    # attributes from attribute to it's value
 
-    def __init__(self, uid: str, attributes: dict[str, str] = None):
-        if attributes is None:
-            attributes = {}
+    def __init__(self, uid: str, attributes: dict[str, str]) -> None:
         self.__uid = uid
         self.attributes = attributes
 
-    def get_uid(self) -> str:
-        """Returns the uid of the participant.
+    @property
+    def uid(self) -> int:
+        """UID of the :class:`Participant`.
 
-        :return: unique id of the participant
+        :return: UID of the :class:`Participant`
         """
         return self.__uid
 
     def get_attribute(self, attribute: str) -> str:
-        """Returns the attribute value of the given attribute for the participant.
+        """Return the attribute value of the given attribute for the :class:`Participant`.
 
-        :param attribute: attribute of which the value is returned
+        :param attribute: Attribute of which the value is returned
 
-        :return: attribute value of the given attribut for the participant
+        :return: Attribute value of the given attribut for the :class:`Participant`
         """
         return self.attributes[attribute]
 
     def set_attribute(self, attribute: str, value: str) -> None:
-        """Sets the given attribute to the given value for the participant.
+        """Set the given attribute to the given value for the :class:`Participant`.
 
-        :param attribute: attribute to be set for participant (already existing)
-        :param value: value given to the given attribute
+        :param attribute: Attribute to be set (already existing)
+        :param value: Value for the attribute
         """
         self.attributes[attribute] = value
 
-    def __eq__(self, other) -> bool:
-        return self.__uid == other.__uid
+    def __eq__(self, other: "Participant") -> bool:
+        return self.__uid == other.uid
 
     def __repr__(self) -> str:
-        return "Participant(" + self.__uid + ", " + str(self.attributes) + ")"
+        return f"Participant({self.__uid}, {self.attributes})"
 
     def __str__(self) -> str:
-        return "Name: " + self.__uid + " Attribute: " + str(self.attributes)
+        return f"UID: {self.__uid} Attributes: {self.attributes}"
 
     def __hash__(self) -> int:
         return self.__uid
@@ -59,3 +59,11 @@ class Participant:
 type Group = set[Participant]
 type Iteration = list[Group]
 type Assignment = list[Iteration]
+
+
+# pylint: disable=invalid-name
+#: Generic type variable
+type T = TypeVar("T")
+
+type ListOfRowLists[T] = list[list[T]]
+type ListOfColumnLists[T] = list[list[T]]
