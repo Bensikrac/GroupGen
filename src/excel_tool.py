6a2ebd328490b83210d3a1ca3a180b4265144d15
Benedek Major <benedek@major.onl>
2024-12-12 15:47:58 +0100

    format: excel tool

--- src/excel_tool.py
+++ src/excel_tool.py
@@ -35,10 +35,8 @@ class Reader:
         for i, entry in enumerate(header_row):
             header_list[i] = entry.value
 
-
         for i in range(2, dataframe_active.max_row):
             p: Participant = Participant(i)
-
 
             for j in range(0, dataframe_active.max_column):
                 p.set_attribute(header_list[j], dataframe_active[i][j].value)
