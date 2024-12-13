a835c5ba435682d54cb1af2984d88272e1065992
Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>
2024-12-13 15:17:55 +0100

    add functionality to ui
    Signed-off-by: Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>

--- src/app.py
+++ src/app.py
@@ -22,22 +22,22 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("GroupGen")
 
         self.input_pick_button.clicked.connect(self.__input_file_picker)
-
         self.output_pick_button.clicked.connect(self.__output_file_picker)
-
         self.run_workflow_button.clicked.connect(self.__run_workflow)
 
-
     def __run_workflow(self) -> None:
+        if self.__input_path is None:
+            raise ValueError("Input Path not set")
+        if self.__output_path is None:
+            raise ValueError("Output Path not set")
+
+        self.state_label.setText("Status: Preparing...")
+
         participant_list: list[Participant]
         algorithm_instance: SimulatedAnnealingAlgorithm
         final_assignment: Assignment
 
-        if self.__input_path is not None:
-            excel_reader: Reader = Reader(self.__input_path)
-        else:
-            raise ValueError("Input Path not set")
-
+        excel_reader: Reader = Reader(self.__input_path)
         self.participant_list = excel_reader.read()
 
         p_set = set(self.participant_list)
@@ -46,31 +46,31 @@ class MainWindow(QMainWindow):
             list(self.participant_list[0].attributes.keys())
         )
 
+        self.state_label.setText("Status: Calculating...")
+
         self.final_assignment = self.algorithm_instance.find_assignment(
             p_set,
-            4,
-            3,
+            int(self.groups_spinbox.value()),
+            int(self.iterations_spinbox.value()),
             50,
         )
-        if self.__output_path is not None:
-            excel_reader: Reader = Reader(self.__output_path)
-        else:
-            raise ValueError("Output Path not set")
 
         excel_writer: Writer = Writer(self.__output_path)
         excel_writer.write_file(self.final_assignment)
 
-    @staticmethod
-    def __file_dialog() -> os.PathLike:
-        return QFileDialog.getOpenFileName(
-            caption="pick file", directory="/home", filter="Excel Files (*.xls *.xlsx)"
-        )[0]
+        self.state_label.setText("Status: Finished!")
 
     def __input_file_picker(self) -> None:
-        self.__input_path = self.__file_dialog()
+        self.__input_path = QFileDialog.getOpenFileName(
+            caption="pick file", directory="/home", filter="Excel Files (*.xls *.xlsx)"
+        )[0]
+        self.input_file_path_line_edit.setText(self.__input_path)
 
     def __output_file_picker(self) -> None:
-        self.__output_path = self.__file_dialog()
+        self.__output_path = QFileDialog.getSaveFileName(
+            caption="pick file", directory="/home", filter="Excel Files (*.xls *.xlsx)"
+        )[0]
+        self.output_file_path_line_edit.setText(self.__output_path)
 
 
 if __name__ == "__main__":
