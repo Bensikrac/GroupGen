65ddffae98bf8c8c4890a5ca2ef30ea27cd2375a
David Schäfer <126971273+VezNan-42@users.noreply.github.com>
2024-12-16 13:42:14 +0100

    17 the algorithm (#64)
    * Add random group generator
    
    * Add simulated annealing algorithm
    
    * Clean up code and make it conform to style guide
    
    * changed the cost function to weighted cost
    
    * changed is to ==
    
    * changed getter of uid to return int, updated __hash__
    
    * paramter in init was a string still
    
    * Fix and improve algorithm and objective function
    
    * Fix algorithm again, add brute force algorithm for comparison
    
    * Fix group shapes
    
    * Clean up code again
    
    * Fix objective function again
    
    * Use comprehension instead of loop, fix typehint and docstring
    
    * Use more helper functions, clean up some details
    
    * Clean up find_neighbor
    
    * fix typehints and use dontcares where appropriate
    
    ---------
    
    Co-authored-by: Moritz <Moritz@breiting.de>

---
+++ src/algorithm/simulated_annealing_algorithm.py
@@ -0,0 +1,211 @@
+"""Simulated Annealing algorithm module"""
+
+from copy import copy
+from math import exp
+from random import Random
+from typing import Any
+from objective_function import ObjectiveFunction
+from random_algorithm import RandomAlgorithm
+from data_structures import Assignment, Iteration, Participant
+
+
+class SimulatedAnnealingAlgorithm:
+    """Contains the calculations for generating group assignments using simulated annealing.
+
+    :param attributes: A list of attributes that are considered for optimization
+    :param random_instance: An instance of Random that will be used
+    instead of an automatically generated one as a source or randomness, defaults to None
+    """
+
+    __random: Random
+    attributes: list[str]
+
+    temperatures: list[float] = []
+    scores: list[float] = []
+
+    def __init__(self, attributes: list[str], random_instance: Random = None):
+        self.__random = Random() if random_instance is None else random_instance
+        self.attributes = attributes
+
+    def find_assignment(
+        self,
+        participants: set[Participant],
+        groups_per_iteration: int,
+        iterations: int,
+        max_cycles: int,
+        intitial_temperature: float = 1,
+        temperature_scaling: float = 15,
+        mix_weight: float = 1,
+        diversity_weight: float = 1,
+    ) -> Assignment:
+        """Return a group assignment generated using simulated annealing.
+
+        :param participants: the set of participants to distribute into groups
+        :param groups_per_iteration: the number of groups in each iteration
+        :param iterations: the total number of iterations
+        :param max_cycles: the maximum number of times
+        the algorithm will iteratively improve the assignment
+        :param intitial_temperature: the initial Temperature, deafults to 1
+        :param temperature_scaling: controls the rate of temperature decay, higher means quicker,
+        defaults to 15
+        :param mix_weight: the weight of the mix cost when evaluating assignments,
+        only the size of this number compared to the diversity weight matters, defaults to 1
+        :param diversity_weight: the weight of the diversity cost, defaults to 1
+
+        :return: the generated assignment
+        """
+        random: RandomAlgorithm = RandomAlgorithm(self.__random)
+        assignment: Assignment = random.find_assignment(
+            participants, groups_per_iteration, iterations
+        )
+        objective: ObjectiveFunction = ObjectiveFunction(self.attributes)
+        for i in range(1, max_cycles + 1):
+            temperature: float = self.get_temperature(
+                i / max_cycles, intitial_temperature, temperature_scaling
+            )
+            neighbor: Assignment = self.find_neighbor(assignment)
+            if self.__should_take_step(
+                assignment,
+                neighbor,
+                temperature,
+                objective,
+                mix_weight,
+                diversity_weight,
+            ):
+                assignment = neighbor
+            self.scores.append(
+                objective.calculate_weighted_cost(
+                    assignment, mix_weight, diversity_weight
+                )
+            )
+        return assignment
+
+    def __should_take_step(
+        self,
+        assignment: Assignment,
+        neighbor: Assignment,
+        temperature: float,
+        objective: ObjectiveFunction,
+        mix_weight: float,
+        diversity_weight: float,
+    ) -> bool:
+        """Determines whether the algorithm takes the step to a given neighbor.
+
+        :param assignment: the current assignment
+        :param neighbor: the neighboring assignment
+        :param temperature: the current temperature
+        :param objective: the objective function to use
+        :param mix_weight: the weight of the mix cost when evaluating assignments,
+        only the size of this number compared to the diversity weight matters
+        :param diversity_weight: the weight of the diversity cost
+
+        :return: true if the steep should be taken, false otherwise
+        """
+        return (
+            self.get_step_probability(
+                objective.calculate_weighted_cost(
+                    assignment, mix_weight, diversity_weight
+                ),
+                objective.calculate_weighted_cost(
+                    neighbor, mix_weight, diversity_weight
+                ),
+                temperature,
+            )
+            >= self.__random.random()
+        )
+
+    def get_temperature(
+        self, progress: float, intitial_temperature: float, scaling: float
+    ) -> float:
+        """Calculate the temperature for a given progress through the annealing process.
+
+        :param progress: The fraction of the maximum cycles for the process that has passed
+        :param intitial_temperature: The initial temperature
+        :param scaling: Controls the rate of exponential decay, higher means quicker decay
+
+        :return: The temperature value
+        """
+
+        temperature: float = (
+            intitial_temperature * (1 - progress) * exp(-scaling * progress)
+        )
+
+        self.temperatures.append(temperature)
+
+        return temperature
+
+    def find_neighbor(self, assignment: Assignment) -> Assignment:
+        """Return a random group assignment that is one swap removed from the given assignment.
+
+        :param assignment: The assignment to find a neighbor for
+
+        :return: The found neighbor
+        """
+        neighbor: Assignment = self.__half_deep_copy(assignment)
+        index = self.__random.randrange(len(assignment))
+        iteration: Iteration = assignment[index]
+        group_index_1: int = self.__random.randrange(len(iteration))
+        participant_index_1: int = self.__random.randrange(
+            len(iteration[group_index_1])
+        )
+
+        group_index_2: int = -1
+        while group_index_2 in (-1, group_index_1):
+            group_index_2 = self.__random.randrange(len(iteration))
+        participant_index_2: int = self.__random.randrange(
+            len(iteration[group_index_2])
+        )
+
+        self.__swap_between_sets(
+            neighbor[index][group_index_1],
+            neighbor[index][group_index_2],
+            list(iteration[group_index_1])[participant_index_1],
+            list(iteration[group_index_2])[participant_index_2],
+        )
+        return neighbor
+
+    def __swap_between_sets(
+        self, set_1: set, set_2: set, element_1: Any, element_2: Any
+    ) -> None:
+        """Swaps two elements between two sets.
+
+        :param set_1: the first set
+        :param set_2: the second set
+        :param element_1: the first element, initially in set_1
+        :param element_2: the second element, initially in set_2
+        """
+        set_1.discard(element_1)
+        set_2.add(element_1)
+        set_2.discard(element_2)
+        set_1.add(element_2)
+
+    def __half_deep_copy(self, obj: Any) -> Any:
+        """Return a recursive deep copy of the given object
+        that stops at any element that isn't a set or list.
+
+        :param obj: The object to copy
+
+        :return: The copy
+        """
+        if isinstance(obj, list):
+            return [self.__half_deep_copy(item) for item in obj]
+        if isinstance(obj, set):
+            return {self.__half_deep_copy(item) for item in obj}
+        return copy(obj)
+
+    def get_step_probability(
+        self, energy_old: float, energy_new: float, temperature: float
+    ) -> float:
+        """Return the probability of taking a step from an assignment to another.
+
+        :param energy_old: The cost of the old assignment
+        :param energy_new: The cost of the new assignment
+        :param temperature: The current temperature
+
+        :return: The probability of taking the step, based on the Metropolis criterion
+        """
+        if energy_new < energy_old:
+            return 1
+        if temperature <= 0:
+            return 0
+        return exp(-(energy_new - energy_old) / temperature)
--- src/data_structures.py
+++ src/data_structures.py
@@ -13,13 +13,13 @@ class Participant:
     attributes: dict[str, str]
     # attributes are mapped using attribute name ex. 'course' -> 'math'
 
-    def __init__(self, uid: str, attributes: dict[str, str] = None):
+    def __init__(self, uid: int, attributes: dict[str, str] = None):
         if attributes is None:
             attributes = {}
         self.__uid = uid
         self.attributes = attributes
 
-    def get_uid(self) -> str:
+    def get_uid(self) -> int:
         """Returns the uid of the participant.
 
         :return: unique id of the participant
@@ -44,7 +44,7 @@ class Participant:
         self.attributes[attribute] = value
 
     def __eq__(self, other) -> bool:
-        return self.__uid == other.__uid
+        return isinstance(other, Participant) and self.get_uid() == other.get_uid()
 
     def __repr__(self) -> str:
         return "Participant(" + self.__uid + ", " + str(self.attributes) + ")"
@@ -53,7 +53,7 @@ class Participant:
         return "Name: " + self.__uid + " Attribute: " + str(self.attributes)
 
     def __hash__(self) -> int:
-        return self.__uid
+        return hash(self.__uid)
 
 
 type Group = set[Participant]
