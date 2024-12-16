0008b6ce8e61a7fbbab08d1172937f50fddfeb63
Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>
2024-12-16 17:26:08 +0100

    fix status display
    Signed-off-by: Tjark Stallmann <tjark.stallmann@stud.tu-darmstadt.de>

--- src/app.py
+++ src/app.py
@@ -27,22 +27,28 @@ class MainWindow(QMainWindow):
         self.output_pick_button.clicked.connect(self.__output_file_picker)
         self.run_workflow_button.clicked.connect(self.__run_workflow)
 
+        self.__set_status("Waiting...")
+
+    def __set_status(self, status: str) -> None:
+        self.state_label.setText(status)
+        self.state_label.repaint()
+
     def __run_workflow(self) -> None:
         if self.__input_path is None:
             raise ValueError("Input Path not set")
         if self.__output_path is None:
             raise ValueError("Output Path not set")
 
-        self.state_label.setText("Status: Preparing...")
+        self.__set_status("Status: Reading file...")
 
         participant_list: list[Participant] = Reader(self.__input_path).read()
         participant_set: set[Participant] = set(participant_list)
 
+        self.__set_status("Status: Calculating...")
+
         algorithm_instance: SimulatedAnnealingAlgorithm = SimulatedAnnealingAlgorithm(
             list(participant_list[0].attributes.keys())
         )
-
-        self.state_label.setText("Status: Calculating...")
 
         final_assignment: Assignment = algorithm_instance.find_assignment(
             participant_set,
@@ -53,7 +59,7 @@ class MainWindow(QMainWindow):
 
         Writer(self.__output_path).write_file(final_assignment)
 
-        self.state_label.setText("Status: Finished!")
+        self.__set_status("Status: Finished!")
 
     def __input_file_picker(self) -> None:
         self.__input_path = QFileDialog.getOpenFileName(
