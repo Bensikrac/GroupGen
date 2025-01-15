28a4612b18ba02b29f6f0fdc23b5d6aae48112f0
Benedek Major <benedek@major.onl>
2025-01-15 23:46:51 +0100

    add PyInstaller scripts and pyproject dependency

---
+++ scripts/winrelease.bat
@@ -0,0 +1,6 @@
+python -m venv venv
+.\venv\Scripts\python.exe -m pip install ".[dev]" ".[gui]" ".[release]"
+.\venv\Scripts\python.exe -m PyQt6.uic.pyuic -o src/assets assets/
+.\venv\Scripts\python.exe -m PyInstaller --noconsole --onefile src/app.py
+rem "Exe is in dist/"
+pause
