3f16251dfbb907e7c262d48e75eebd9c47b014a8
David Schäfer <david@schaefer.su>
2025-02-06 17:14:47 +0100

    Placate lint

--- src/app.py
+++ src/app.py
@@ -344,10 +344,14 @@ def asset_path(relative_path) -> str:
     :param relative_path: The relative path to the assset
     :return: The absolute path
     """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
+    # pylint: disable=locally-disabled, protected-access, broad-exception-caught
+    if sys.platform.startswith("win32"):
+        try:
+            # PyInstaller creates a temp folder and stores path in _MEIPASS
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = "assets/"
+    else:
         base_path = "assets/"
 
     return os.path.join(base_path, relative_path)
